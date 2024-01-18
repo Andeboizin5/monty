2da10f6b8d594d775ad41cf427d1e37a016d1fdb
Andeboizin5 <andeboizin5@gmail.com>
2024-01-18 05:18:19 -0800

    _pushpall.c

--- monty.h
+++ monty.h
@@ -12,6 +12,12 @@
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
 void push(stack_t **stack, int value);
 void pall(stack_t *stack);
 
