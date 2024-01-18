40106fd73acd0a12d51c03b3981ba50b99e5d0b3
Andeboizin5 <andeboizin5@gmail.com>
2024-01-18 07:09:05 -0800

    _pushpall.c

--- monty.h
+++ monty.h
@@ -52,8 +52,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void push(stack_t **stack, int value);
-void pall(stack_t *stack);
-
+void _push(stack_t **head, unsigned int counter)
+void _pall(stack_t **head, unsigned int count)
 
 #endif
